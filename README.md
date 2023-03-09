e62a72f6d7a71384ba6c02c12ebd6ff602e498a0
IdoUziyahu <idouziyahu2002@gmail.com>
2023-03-09 12:54:41 +0200

    the first GIGA update

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # GigaButton
 a button that plays gigachad's theme
+![GIGACHAD](./assets/GIGACHAD.jpg)
---
+++ assets/main.js
@@ -0,0 +1,26 @@
+const audio = new Audio('./assets/songLoop.mp3')
+const button = document.getElementById('giga-button')
+const body = document.getElementsByTagName('body')[0]
+
+
+const enableSong = () => {
+    audio.play()
+    audio.loop = true
+    body.classList.add('activated');
+}
+
+const disableSong = () => {
+    audio.pause()
+    audio.currentTime = 0;
+    body.classList.remove('activated');
+
+}
+
+button.addEventListener('mousedown', enableSong);
+button.addEventListener('touchstart', enableSong);
+
+
+button.addEventListener('mouseup', disableSong);
+button.addEventListener('mouseout', disableSong);
+button.addEventListener('touchstop', disableSong);
+button.addEventListener('touchcancel', disableSong);
